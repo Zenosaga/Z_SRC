9b591f4ff57d9d3d39310c9cf80e1d93161fd73f
int <zenosaga1@gmail.com>
2014-07-27 20:56:46 -0700

    Create directory.php
    This file contains the template for the "Directory" as it appears on the lower half of the Zenosaga main page.

---
+++ global/templates/directory.php
@@ -0,0 +1,167 @@
+
+<!-- BEGIN DIRECTORY -->
+
+<table width="548" border="0" cellspacing="2" cellpadding="0" align="right">
+
+<tr> 
+<td width="273" height="16" class=dirheadline>
+FAQ
+</td>
+
+<td width="273" height="16" class=dirheadline>
+#bash.ZENOSAGA
+</td>
+</tr>
+
+<tr> 
+<td width="273"> <a href="/faq/?">
+<img src="<?echo IMAGE?>dir-faq.gif" width="265" height="64" border="0">
+</a> 
+</td>
+
+<td width="273">
+<img src="<?echo IMAGE?>dir-bash.gif" width="265" height="64" border="0">
+</td>
+</tr>
+
+<tr valign="top"> 
+<td width="273" class=defaulttext>
+<a href="/faq/?catID=4">EHQ Site related questions</a><br>
+<a href="/faq/?catID=2">Xenogears related questions</a><br>
+<a href="/faq/?catID=3">Xenosaga related questions</a><br>
+</td>
+
+<td width="273" class=defaulttext>
+Bloopers and miscellaneous files<br>
+IRC chat quotes and logs<br>
+Members' Lounge at Z Lobby<br>
+</td>
+</tr>
+
+<tr> 
+<td width="273">&nbsp;</td>
+<td width="273">&nbsp;</td>
+</tr>
+
+<tr> 
+<td width="273" class=dirheadline>
+Gallery
+</td>
+
+<td width="273" class=dirheadline>
+Library
+</td>
+</tr>
+
+<tr> 
+<td width="273">
+<a href="http://gallery.zenosaga.com/">
+<img src="<?echo IMAGE?>dir-gall.gif" width="265" height="64" border="0">
+</a>
+</td>
+
+<td width="273">
+<a href="http://umn.zenosaga.com/elsa/library/?">
+<img src="<?echo IMAGE?>dir-lib.gif" width="265" height="64" border="0">
+</a>
+</td>
+</tr>
+
+<tr valign="top"> 
+<td width="273" class=defaulttext>
+<a href = "http://gallery.zenosaga.com/index.php?redir=http://gallery.zenosaga.com/cgi-bin/imageFolio.cgi?direct=EPISODE_I">Xenosaga image archive</a><br>
+<a href = "http://gallery.zenosaga.com/index.php?redir=http://gallery.zenosaga.com/cgi-bin/imageFolio.cgi?direct=EPISODE_V_XG">Xenogears image archives</a><br>
+FMV image archives<br>
+</td>
+
+<td width="273" class=defaulttext>
+<a href="http://umn.zenosaga.com/elsa/library/archive.php">Browse fan art library</a><br>
+Browse fan fic library<br>
+<a href="http://umn.zenosaga.com/elsa/library/up_art.php">Submit your works to the 'ETHOS'</a><br>
+</td>
+</tr>
+
+<tr> 
+<td width="273">&nbsp;
+
+</td>
+
+<td width="273">&nbsp;
+
+</td>
+</tr>
+
+<tr> 
+<td width="273" class=dirheadline>
+'ETHOS' Complete Works
+</td>
+
+<td width="273" class=dirheadline>
+SOL-9000</td>
+</tr>
+
+<tr> 
+<td width="273">
+<img src="<?echo IMAGE?>dir-hist.gif" width="265" height="64">
+</td>
+
+<td width="273"> <a href="http://umn.zenosaga.com/sol9k/"><img src="<?echo IMAGE?>dir-sol9.gif" border="0"></a> 
+</td>
+</tr>
+
+<tr valign="top"> 
+<td width="273" class=defaulttext>
+Pre-aXGSite era<br>
+Age of aFF8Site / FFOmega<br>
+Concept of EHQ<br>
+</td>
+
+<td width="273" class=defaulttext> <a href="http://umn.zenosaga.com/sol9k/">Browse the SOL-9000 database</a><br>
+<a href="http://umn.zenosaga.com/sol9k/result.php?eID=1&type=episode">Xenosaga Episode I</a><br>
+<a href="http://umn.zenosaga.com/sol9k/result.php?eID=5&type=episode">Xenogears Episode V</a><br>
+</td>
+</tr>
+
+<tr> 
+<td width="273">&nbsp;
+
+</td>
+
+<td width="273">&nbsp;
+
+</td>
+</tr>
+
+<tr> 
+<td width="273" class=dirheadline>&nbsp;</td>
+
+<td width="273">
+<span class=dirheadline>
+Downloads
+</span>
+</td>
+</tr>
+
+<tr> 
+<td width="273" valign="top">&nbsp;</td>
+<td width="273">
+<a href="http://download.zenosaga.com/">
+<img src="<?echo IMAGE?>dir-dnld.gif" width="265" height="64" border="0">
+</a>
+</td>
+</tr>
+
+<tr> 
+<td width="273" valign="top">&nbsp;</td>
+
+<td width="273" class=defaulttext>
+
+<a href="http://download.zenosaga.com/download/search_results.php?download_type_id=2">Movies and promotions</a><br>
+<a href="http://download.zenosaga.com/download/search_results.php?download_type_id=4">Xenogears/saga wallpapers</a><br>
+<a href="http://download.zenosaga.com/download/search_results.php?download_type_id=5">X-tras (images, scans, etc.)</a><br>
+</td>
+</tr>
+</table>
+
+<!-- //END DIRECTORY -->
+
