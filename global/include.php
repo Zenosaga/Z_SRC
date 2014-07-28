a59e8bfdc5552e1c101c88e14d86ddbffc4d23d4
int <zenosaga1@gmail.com>
2014-07-27 21:10:16 -0700

    Create include.php
    This file is the "master" include file, calling includes to all files that include essential functions.

---
+++ global/include.php
@@ -0,0 +1,54 @@
+<?
+// *********************************
+// Object classes
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/cls_page.php");
+	require_once("/zenosaga.com/httpdocs/global/cls_subs.php");
+	require_once("/zenosaga.com/httpdocs/global/cls_char.php");
+
+// *********************************
+// Global variables
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/vars.php");
+	require_once("/zenosaga.com/httpdocs/global/db_config.php");
+ 
+// *********************************
+// Browser related functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/agent_fns.php");
+
+// *********************************
+// Cookie functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/ckie_fns.php");
+ 
+// *********************************
+// Database functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/db_fns.php");
+
+// *********************************
+// Error & debugging functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/err_fns.php");
+
+// *********************************
+// Miscellaneous functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/misc_fns.php");
+
+// *********************************
+// Registration functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/regi_fns.php");
+
+// *********************************
+// URL manipulation functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/url_fns.php");
+
+// *********************************
+// User functions
+// *********************************
+	require_once("/zenosaga.com/httpdocs/global/user_fns.php");
+?>
