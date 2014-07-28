9da9e392957cfcd3dd4a377191967c4dee578c11
int <zenosaga1@gmail.com>
2014-07-27 21:27:10 -0700

    Create color.php
    Color designation. This file is used in conjunction with My.ETHOS preference session registrations.

---
+++ global/color.php
@@ -0,0 +1,24 @@
+<?
+	switch ($color)
+	{
+		case 'blue':
+			$ccolor = "blue";
+			$tdcolor = "#466993";
+			break;
+
+		case 'green':
+			$ccolor = "green";
+			$tdcolor = "#275F20";
+			break;
+
+		case 'red':
+			$ccolor = "red";
+			$tdcolor = "#692D2D";
+			break;
+
+		default:
+			$ccolor = "blue";
+			$tdcolor = "#466993";
+			break;
+	}
+?>
